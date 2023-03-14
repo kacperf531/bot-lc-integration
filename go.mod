a2a2bf3ed2ce7f63dead2290ef71f5c28ad5f07b
kacperf531 <k.frackowski@livechatinc.com>
2023-03-14 13:41:05 +0100

    Bot now sends rich message on incoming_chat event

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/kacperf531/bot-lc-integration
 
 go 1.20
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require github.com/joho/godotenv v1.5.1
--- server.go
+++ server.go
@@ -15,20 +15,28 @@ type BotServer struct {
 	HttpClient http.Client
 }
 
-type Event struct {
+type MessageEvent struct {
 	Text string `json:"text"`
 	Type string `json:"type"`
 }
 
 type SendEventRequest struct {
-	ChatID string `json:"chat_id"`
-	Event  Event  `json:"event"`
+	ChatID string          `json:"chat_id"`
+	Event  json.RawMessage `json:"event"`
 }
 
 type IncomingEvent struct {
-	ChatID   string `json:"chat_id"`
-	ThreadID string `json:"thread_id"`
-	Event    Event  `json:"event"`
+	ChatID   string          `json:"chat_id"`
+	ThreadID string          `json:"thread_id"`
+	Event    json.RawMessage `json:"event"`
+}
+
+type Chat struct {
+	ID string `json:"id"`
+}
+
+type IncomingChat struct {
+	Chat Chat `json:"chat"`
 }
 
 type Webhook struct {
@@ -36,7 +44,7 @@ type Webhook struct {
 	Payload json.RawMessage `json:"payload"`
 }
 
-func parseIncomingEvent(payload json.RawMessage) (IncomingEvent, error) {
+func unmarshalIncomingEvent(payload json.RawMessage) (IncomingEvent, error) {
 	var ie IncomingEvent
 	err := json.Unmarshal(payload, &ie)
 	if err != nil {
@@ -45,16 +53,36 @@ func parseIncomingEvent(payload json.RawMessage) (IncomingEvent, error) {
 	return ie, nil
 }
 
+func unmarshalIncomingChat(payload json.RawMessage) (IncomingChat, error) {
+	var ic IncomingChat
+	err := json.Unmarshal(payload, &ic)
+	if err != nil {
+		return ic, err
+	}
+	return ic, nil
+}
+
+func unmarshalEvent(payload json.RawMessage) (MessageEvent, error) {
+	var e MessageEvent
+	err := json.Unmarshal(payload, &e)
+	if err != nil {
+		return e, err
+	}
+	return e, nil
+}
+
 func (bs *BotServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		var w Webhook
 		json.NewDecoder(r.Body).Decode(&w)
-		if w.Action == "incoming_event" {
-			incomingEvent, err := parseIncomingEvent(w.Payload)
-			if err != nil {
-				log.Fatalf("There was a problem when parsing the webhook %+v", w)
-			}
-			bs.SendEventReply(incomingEvent.Event.Text, incomingEvent.ChatID)
+		switch {
+		case w.Action == "incoming_event":
+			incomingEvent, _ := unmarshalIncomingEvent(w.Payload)
+			event, _ := unmarshalEvent(incomingEvent.Event)
+			bs.SendEventReply(event, incomingEvent.ChatID)
+		case w.Action == "incoming_chat":
+			incomingChat, _ := unmarshalIncomingChat(w.Payload)
+			bs.SendRichMessage(incomingChat.Chat.ID)
 		}
 	}
 
@@ -65,27 +93,38 @@ func (bs *BotServer) sendRequest(url string, payload []byte) (*http.Response, er
 	r.Header.Set("Authorization", os.Getenv("TOKEN"))
 	r.Header.Set("Content-type", "Application/json")
 	r.Header.Set("X-Author-Id", os.Getenv("BOT_ID"))
-	return bs.HttpClient.Do(r)
-}
-
-func (bs *BotServer) SendEventReply(prompt, chatId string) {
-	text := "foo"
-	if prompt == "say something else" {
-		text = "bar"
-	}
-
-	replyEvent := SendEventRequest{ChatID: chatId,
-		Event: Event{
-			Text: text,
-			Type: "message"}}
-	requestBody, _ := json.Marshal(replyEvent)
-	response, err := bs.sendRequest(sendEventURL, requestBody)
-	if err != nil {
-		log.Printf(`There was an error sending the "send_event" request, %v`, err)
-	}
+	response, err := bs.HttpClient.Do(r)
 	if response.StatusCode != 200 {
 		responseBody, _ := ioutil.ReadAll(response.Body)
-		log.Fatalf("Livechat API rejected send_event request with message: %s", string(responseBody))
+		log.Fatalf("Livechat API rejected %s request with message: %s", url, string(responseBody))
 	}
+	return response, err
+}
 
+func (bs *BotServer) SendEventReply(event MessageEvent, chatId string) {
+	var text string
+	switch {
+	case event.Text == "I'm just browsing":
+		text = "Sure, let me know if you have any questions."
+	case event.Text == "I'd rather talk with the agent":
+		text = "Granted, you will be redirected to talk with the agent now."
+		// TODO: Perform transfer to agent
+	default:
+		text = "Ok what would you like to talk about?"
+	}
+	message, _ := json.Marshal(MessageEvent{
+		Text: text,
+		Type: "message"})
+	replyEvent := SendEventRequest{ChatID: chatId,
+		Event: message}
+	requestBody, _ := json.Marshal(replyEvent)
+	bs.sendRequest(sendEventURL, requestBody)
+}
+
+func (bs *BotServer) SendRichMessage(chatId string) {
+	richMessage, _ := ioutil.ReadFile("./rich_message.json")
+	replyEvent := SendEventRequest{ChatID: chatId,
+		Event: richMessage}
+	requestBody, _ := json.Marshal(replyEvent)
+	bs.sendRequest(sendEventURL, requestBody)
 }
